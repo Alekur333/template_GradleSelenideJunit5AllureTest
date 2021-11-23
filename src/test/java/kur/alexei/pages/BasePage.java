c70cd6665c209935fc6353649a1076b1cfd8ee13
Alekur333 <amastar963@gmail.com>
2021-11-23 12:14:12 +0500

    added attach, beforeEach in BasePage

--- src/test/java/kur/alexei/pages/BasePage.java
+++ src/test/java/kur/alexei/pages/BasePage.java
@@ -2,7 +2,11 @@ package kur.alexei.pages;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Attachment;
+import io.qameta.allure.selenide.AllureSelenide;
+import kur.alexei.helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -15,14 +19,16 @@ public class BasePage {
 
         Configuration.browserSize="1200x800";
 //        Configuration.proxyEnabled = true; // только для отдельных тестов, а не в общие настройки
-//        Configuration.fileDownload = FileDownloadMode.PROXY; // только для отдельных тестов, а не в общие настройки
+//        Configuration.fileDownload = FileDownloadMode.PROXY; // для скачивания файлов без href, только для отдельных тестов, а не в общие настройки
 
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
-    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
-    public byte[] takeScreenshot() {
-
-        final WebDriver driver = WebDriverRunner.getWebDriver();
-        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
